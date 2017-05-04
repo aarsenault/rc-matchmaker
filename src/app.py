011568926ad6a62a6d8ff86eeef685d6c17db1b2
Adriel Arsenault <adriel.arsenault@gmail.com>
2017-05-03 20:46:44 -0400

    pushing to heroku

--- src/app.py
+++ src/app.py
@@ -2,17 +2,19 @@
 # database
 
 import os
+from functools import wraps
 
-from flask import Flask, render_template
+from flask import Flask, flash, render_template, redirect, request, session, url_for
 from flask_sqlalchemy import SQLAlchemy
+from flask_oauthlib.client import OAuth, OAuthException
 
-from models.models import db
+# from models.models import db
 
 # import matching
 # from users import fetch_users
 # from models import User
-from models import matching
-from models.users import fetch_users
+# from models import matching
+# from models.users import fetch_users
 # from src.models.models import User
 
 app = Flask(__name__)
@@ -21,41 +23,144 @@ app = Flask(__name__)
 app.config.from_object(os.environ['APP_SETTINGS'])
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 # Sanity Checker for env variables
-print( 'Environment configuratino is: {}'.format( os.environ['APP_SETTINGS']) )
+print('Environment configuration is: {}'.format( os.environ['APP_SETTINGS']) )
+print('RC_CLIENT_ID', os.environ.get('RC_CLIENT_ID', ''))
+print('RC_SECRET', os.environ.get('RC_SECRET', ''))
+
+
+oauth = OAuth(app)
+rc_auth = oauth.remote_app(
+
+    # for local
+
+    # 'recurse_center',
+    # base_url='https://www.recurse.com/api/v1/',
+    # access_token_url='https://www.recurse.com/oauth/token',
+    # authorize_url='https://www.recurse.com/oauth/authorize',
+    # consumer_key=os.environ.get('RC_CLIENT_ID', None),
+    # consumer_secret=os.environ.get('RC_SECRET', None),
+    # access_token_method='POST')rc_auth = oauth.remote_app(
+
+    # for online dev
+
+    'recurse_center',
+    base_url='https://www.recurse.com/api/v1/',
+    access_token_url='https://www.recurse.com/oauth/token',
+    authorize_url='https://www.recurse.com/oauth/authorize',
+    consumer_key= 'c412064aa2fac22c3ae4a12157a935dc3ece3476387160cf5a41612055c85e8e',
+    consumer_secret= '5b2603dc1971e895efe5d1e45a2cf3bd044a24197000540d3bf91ce5ab83ecd9',
+    access_token_method='POST')
+
+
+def get_login():
+    return session.get('login')
+
+
+@rc_auth.tokengetter
+def get_rc_oauth_token():
+    return get_login()['oauth_token']
+
+
+def protected(route):
+    # in large apps it is probably better to use the Flask-Login extension than
+    # this route decorator because this decorator doesn't provide you with
+    # 1. user access levels or
+    # 2. the helpful abstraction of an "anonymous" user (not yet logged in)
+    @wraps(route)
+    def wrapper(*args, **kwargs):
+        kwargs.update(login=get_login())
+        return route(*args, **kwargs) if kwargs['login'] \
+            else redirect(url_for('login', next=request.url))
+        # redirect includes "next=request.url" so that after logging in the
+        # user will be sent to the page they were trying to access
+    return wrapper
+
+
+@app.route('/login')
+def login():
+    print('View: login')
+    if get_login():
+        flash('You are already logged in.')
+        return redirect(request.referrer or url_for('index'))
+    else:
+        afterward = request.args.get('next') or request.referrer or None
+        print(afterward)
+        landing = url_for('oauth_authorized', next=afterward, _external=True)
+        print(landing)
+        return rc_auth.authorize(callback=landing)
+
+
+@app.route('/test')
+# @app.route('/oauth_authorized')
+@rc_auth.authorized_handler
+def oauth_authorized(resp):
+    print('View: oauth_authorized')
+    # print(resp.keys())
+    try:
+        # make a partial login session here, get the username later if this part works
+        # keys into resp are probably different for different oauth providers, unfortunately
+        session['login'] = dict(oauth_token=(resp['access_token'], resp['refresh_token']))
+    except TypeError as exc:
+        flash('The login request was gracefully declined. (TypeError: %s)' % exc)
+        return redirect(url_for('index'))
+    except KeyError as exc:
+        flash('There was a problem with the response dictionary. (KeyError: %s) %s' % (exc, resp))
+        return redirect(url_for('index'))
+    # now get their username
+    me = rc_auth.get('people/me')
+    print(me)
+    if me.status == 200:
+        session['login']['user'] = '{first_name} {last_name}'.format(**me.data)
+        session['login']['email'] = me.data['email']
+        session['login']['image'] = me.data['image']
+    else:
+        session['login']['user'] = 'Hacker Schooler'
+    flash('You are logged in.')
+    print(me.data)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    # the important bit here is to remove the login from the session
+    flash('You have logged out.') if session.pop('login', None) \
+        else flash('You aren\'t even logged in.')
+    return redirect(url_for('index'))
 
 
 @app.route('/')
 def index():
-    return  render_template('home.html')
+    return render_template('home.html', login=get_login())
 
 
-@app.route('/matches')
-def show_matches():
-    # TODO: redirect to /coffee
-
-    # Backend: Create list of "users"
-    # Frontend: # List all users
-    users = fetch_users()
-
-    # Backend: stub out a pairing algorithm
-    # Frontend: List current pairings
-    matched_users, unmatched_users = matching.match_users(users)
-
-    # Backend: create algorithm + create tests
-
-    return render_template('test_coffee.html',
-                           users=users,
-                           matched_users=matched_users,
-                           unmatched_users=unmatched_users)
+# @app.route('/matches')
+# def show_matches():
+#     # TODO: redirect to /coffee
+#
+#     # Backend: Create list of "users"
+#     # Frontend: # List all users
+#     users = fetch_users()
+#
+#     # Backend: stub out a pairing algorithm
+#     # Frontend: List current pairings
+#     matched_users, unmatched_users = matching.match_users(users)
+#
+#     # Backend: create algorithm + create tests
+#
+#     return render_template('test_coffee.html',
+#                            users=users,
+#                            matched_users=matched_users,
+#                            unmatched_users=unmatched_users)
 
 
 @app.route('/profile')
-def show_profile():
-    return render_template('profile.html')
+@protected
+def show_profile(login=None):
+    print('View: profile')
+    return render_template('profile.html', login=login)
     # return "Welcome to the Coffee Matchmaker!"
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0')
